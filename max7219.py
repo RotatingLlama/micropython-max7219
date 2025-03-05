859075e17066aa7fe9948cafacfee36cd9b054bd
RotatingLlama <137008229+RotatingLlama@users.noreply.github.com>
2025-03-05 23:41:33 +0000

    Update max7219.py
    Added class inheritance
    Flipped byte order from MONO_HLSB to MONO_HMSB because everything was appearing backwards
    Added power() method to take advantage of 7219 shutdown mode

--- max7219.py
+++ max7219.py
@@ -4,6 +4,7 @@ https://github.com/mcauser/micropython-max7219
 
 MIT License
 Copyright (c) 2017 Mike Causer
+Copyright (c) 2025 Tom Lloyd
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -35,7 +36,7 @@ _SCANLIMIT = const(11)
 _SHUTDOWN = const(12)
 _DISPLAYTEST = const(15)
 
-class Matrix8x8:
+class Matrix8x8(framebuf.FrameBuffer):
     def __init__(self, spi, cs, num):
         """
         Driver for cascading MAX7219 8x8 LED matrices.
@@ -53,27 +54,18 @@ class Matrix8x8:
         self.cs.init(cs.OUT, True)
         self.buffer = bytearray(8 * num)
         self.num = num
-        fb = framebuf.FrameBuffer(self.buffer, 8 * num, 8, framebuf.MONO_HLSB)
-        self.framebuf = fb
-        # Provide methods for accessing FrameBuffer graphics primitives. This is a workround
-        # because inheritance from a native class is currently unsupported.
-        # http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
-        self.fill = fb.fill  # (col)
-        self.pixel = fb.pixel # (x, y[, c])
-        self.hline = fb.hline  # (x, y, w, col)
-        self.vline = fb.vline  # (x, y, h, col)
-        self.line = fb.line  # (x1, y1, x2, y2, col)
-        self.rect = fb.rect  # (x, y, w, h, col)
-        self.fill_rect = fb.fill_rect  # (x, y, w, h, col)
-        self.text = fb.text  # (string, x, y, col=1)
-        self.scroll = fb.scroll  # (dx, dy)
-        self.blit = fb.blit  # (fbuf, x, y[, key])
+        super().__init__(
+            self.buffer,
+            8 * num,
+            8,
+            framebuf.MONO_HMSB
+        )
         self.init()
 
     def _write(self, command, data):
         self.cs(0)
         for m in range(self.num):
-            self.spi.write(bytearray([command, data]))
+            self.spi.write(bytes([command, data]))
         self.cs(1)
 
     def init(self):
@@ -90,7 +82,12 @@ class Matrix8x8:
         if not 0 <= value <= 15:
             raise ValueError("Brightness out of range")
         self._write(_INTENSITY, value)
-
+    
+    def power(self, p):
+        if p not in (0,1):
+            raise ValueError("power() needs 0 or 1")
+        self._write(_SHUTDOWN, p)
+    
     def show(self):
         for y in range(8):
             self.cs(0)
